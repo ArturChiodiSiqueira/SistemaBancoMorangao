cd3ebe9162a792bd30ad58e18f7b1a1273eb237b
artur <artur@DESKTOP-V0JU5PP>
2022-08-30 09:59:55 -0300

    criacao das propriedades e construtores

---
+++ SistemaBancoMorangao/Endereco.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancoMorangao
+{
+    internal class Endereco
+    {
+        public String Logradouro { get; set; }
+        public String Numero { get; set; }
+        public String Bairro { get; set; }
+        public String Cidade { get; set; }
+        public String Cep { get; set; }
+        public String Complemento { get; set; }
+
+        public Endereco()
+        {
+
+        }
+
+        public Endereco(string logradouro, string numero, string bairro, string cidade, string cep, string complemento)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Bairro = bairro;
+            Cidade = cidade;
+            Cep = cep;
+            Complemento = complemento;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
