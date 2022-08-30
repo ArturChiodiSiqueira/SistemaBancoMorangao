934303b9d9c87503b404442a99db68d3f4f6388b
artur <artur@DESKTOP-V0JU5PP>
2022-08-30 14:43:52 -0300

    criacao menus

--- SistemaBancoMorangao/Program.cs
+++ SistemaBancoMorangao/Program.cs
@@ -8,19 +8,107 @@ namespace SistemaBancoMorangao
         {
             Console.Title = "SISTEMA BANCO MORANGÃO";
 
-            MostrarMenu();
+            MostrarMenuInicial();
         }
 
-        static void MostrarMenu()
+        static void MostrarMenuInicial()
         {
             string opcao;
 
             do
             {
                 Console.Clear();
-                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("\nQUAL SEU TIPO DE ACESSO COM BASE NAS OPÇÕES?\n");
+                Console.WriteLine("\t|°°°°°°°°°°°°°  MENU  INICIAL  °°°°°°°°°°°°°|");
+                Console.WriteLine("\t|   opção 0 : funcionario                   |");
+                Console.WriteLine("\t|                                           |");
+                Console.WriteLine("\t|   opção 1 : cliente                       |");
+                Console.WriteLine("\t|___________________________________________|");
+
+                Console.Write("\n\tInforme a opcao: ");
+                opcao = Console.ReadLine();
+
+                if (opcao != "0" && opcao != "1" && opcao != "2")
+                {
+                    Console.WriteLine("'" + opcao + "' é uma opcao INVALIDA! Para voltar ao MENU, pressione QUALQUER TECLA!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                else
+                {
+                    switch (opcao)
+                    {
+                        case "0":
+                            Console.Clear();
+                            MostrarMenuFuncionario();
+                            break;
+
+                        case "1":
+                            Console.Clear();
+                            MostrarMenuCliente();
+                            break;
+                    }
+                }
+            } while (opcao != "0");
+        }
+
+        static void MostrarMenuFuncionario()
+        {
+            string opcao;
+
+            do
+            {
+                Console.Clear();
                 Console.WriteLine("\nDENTRE AS OPÇÕES NO MENU, QUAL DESEJA EXECUTAR?\n");
-                Console.WriteLine("\t|°°°°°°°°°°°°°°°°°  MENU  °°°°°°°°°°°°°°°°°°|");
+                Console.WriteLine("\t|°°°°°°°°°°°  MENU  FUNCIONARIO  °°°°°°°°°°°|");
+                Console.WriteLine("\t|   opção 0 : sair                          |");
+                Console.WriteLine("\t|                                           |");
+                Console.WriteLine("\t|   opção 1 : nivel de acesso funcionario   |");
+                Console.WriteLine("\t|                                           |");
+                Console.WriteLine("\t|   opção 2 : nivel de acesso gerente       |");
+                Console.WriteLine("\t|___________________________________________|");
+
+                Console.Write("\n\tInforme a opcao: ");
+                opcao = Console.ReadLine();
+
+                if (opcao != "0" && opcao != "1" && opcao != "2")
+                {
+                    Console.WriteLine("'" + opcao + "' é uma opcao INVALIDA! Para voltar ao MENU, pressione QUALQUER TECLA!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                else
+                {
+                    switch (opcao)
+                    {
+                        case "0":
+                            Console.WriteLine("\nVOCÊ ESCOLHEU SAIR.");
+                            break;
+                        case "1":
+                            Console.Clear();
+
+                            break;
+
+                        case "2":
+                            Console.Clear();
+
+                            break;
+                    }
+                }
+            } while (opcao != "0");
+        }
+
+        static void MostrarMenuCliente()
+        {
+            string opcao;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\nDENTRE AS OPÇÕES NO MENU, QUAL DESEJA EXECUTAR?\n");
+                Console.WriteLine("\t|°°°°°°°°°°°°°  MENU  CLIENTE  °°°°°°°°°°°°°|");
                 Console.WriteLine("\t|   opção 0 : sair                          |");
                 Console.WriteLine("\t|                                           |");
                 Console.WriteLine("\t|   opção 1 : solicitar abertura de conta   |");
