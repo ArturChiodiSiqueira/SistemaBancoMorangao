037db45d8c08456f14d2a79661aef0885633c9ea
artur <artur@DESKTOP-V0JU5PP>
2022-08-30 11:30:51 -0300

    criacao

---
+++ SistemaBancoMorangao/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancoMorangao
+{
+    internal class Cliente
+    {
+        public bool Habilitacao { get; set; }
+
+    }
+}
